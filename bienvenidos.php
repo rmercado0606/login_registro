99b432c4fe6292e45a89431d8a1a36c91698339a
Rafael Mercado <rmercado0606@hotmail.com>
2023-08-25 18:26:11 -0500

    login y registro

---
+++ bienvenidos.php
@@ -0,0 +1,33 @@
+<?php
+
+    session_start();
+
+    if(!isset($_SESSION['usuario'])){
+        echo '
+
+            <script>
+                alert("Por favor dedes iniciar sesión");
+                window.location = "index.php";       
+            </script>
+        ';
+        session_destroy();
+        die();
+    }
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>bienvenidos</title>
+</head>
+<body>
+    <h1>bienvenido a mi mundo</h1>
+    <a href="php/cerrar_session.php">cerrar sesión</a>
+    
+</body>
+</html>
---
+++ php/conexion_be.php
@@ -0,0 +1,11 @@
+<?php
+
+    $conexion = mysqli_connect("localhost", "root", "", "login_register_db");
+    /*
+    if($conexion){
+        echo 'conectado exitosamente a la Base de datos';
+    }else{
+        echo ' No se ha podido conectar a la Base de datos';
+    }
+    */
+?>
